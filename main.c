5b50f72d8c2184ec8cffaf2b1374621ed9fd6d9f
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-08-18 08:38:11 +0000

    Add and fix some prototypes

--- main.c
+++ main.c
@@ -1,35 +1,70 @@
 #include "main.h"
-#define INIT_BUFFER_SIZE 5
 
 /**
-* init_env - init the global environment variable
-* @env: int from this env
+* handle_separators - handle separators (tell if the next command should be 
+* executed or not)
+* @prev_result: previous command result (-number is error, 1 is passed,
+* prev_result shouldn't have the value zero but if it is then will return 0)
+* @separator: the separator between the commands ex. && or || or ;
+* Return: one if the next command should be executed and zero otherwise
 */
-void init_env(char **env)
+int handle_separators(int prev_result, char *separator)
+{}
+
+/**
+* handle_command - handle one command only
+* @command: the command
+* @env: the environment list
+* @alias: the alias list
+* Return: zero if the command tell the shell to exit (exit interactive loop)
+* and negative if there is an error (look at error ids)
+* and one otherwise
+*/
+int handle_command(char **command, Item *env, Item *alias)
+{}
+
+/**
+* filter_commands - filter the commands from comments and separators
+* @commands: one line of commands
+* @Return: list of commands separated by separators
+* ex. ls;ls||ls -> [ls, ;, ls, ||, ls]
+*/
+char **filter_commands(char *commands)
+{}
+
+/**
+* handle_commands - take a line of commands and handle them
+* @commands: the line of commands
+* @env: the environment list
+* @alias: the alias list
+* @program_name: the program name (argv[0])
+* Return: zero if the commands tell the shell to exit (exit interactive loop)
+* and one otherwise
+*/
+int handle_commands(char *commands, Item *env, Item *alias, char *program_name)
 {
-	
+
 }
 
-
-int handle_commands(char *str)
-{
-	printf("===\n%s===\n", str);
-	return (1);
-}
 
 /**
 * main - The main function
 */
-int main(int argc, char **argv, char **env)
+int main(int argc, char **argv, char **_env)
 {
 	char *buffer = NULL;
 	size_t buffer_size = INIT_BUFFER_SIZE;
 	int bytes_read = 0;
 	int still_loop = 0;
+	Item *env = NULL, *alias = NULL;
 
 	buffer = malloc(sizeof(char) * buffer_size);
 	if (!buffer)
 		return (0);
+
+	/*
+	alias = init_alias();
+	env = init_env(_env);*/
 
 	still_loop = isatty(STDIN_FILENO);
 	if (!still_loop) /*Non interactive mode*/
@@ -40,10 +75,10 @@ int main(int argc, char **argv, char **env)
 			free(buffer);
 			return (0);
 		}
-		handle_commands(buffer);
+		handle_commands(buffer, env, alias, argv[0]);
 	}
 
-	while (still_loop)
+	while (still_loop) /*Interactive mode*/
 	{
 		write(1, "($) ", 4);
 		bytes_read = getline(&buffer, &buffer_size, stdin);
@@ -52,7 +87,7 @@ int main(int argc, char **argv, char **env)
 			free(buffer);
 			return (0);
 		}
-		still_loop = handle_commands(buffer);
+		still_loop = handle_commands(buffer, env, alias, argv[0]);
 	}
 	free(buffer);
 	return (0);
--- main.h
+++ main.h
@@ -5,14 +5,17 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stddef.h>
+
 #define COMMAND_BUILT_IN 1
 #define COMMAND_PATH 2
+#define MAX_LINE_LENGTH 512
+#define INIT_BUFFER_SIZE 5
 
-/* Error ids */
-#define E_COMMAND 1
-#define E_PATH 2
-#define E_ACCESS 3
-
+/* Error ids (all are must be negative) */
+#define E_COMMAND_UNKNOWN -1
+#define E_PATH_NOT_EXIST -2
+#define E_ACCESS_FILE -3
+#define E_FILE_RETURN_E -4 /* executed file return error (-1) */
 
 /**
 * item - General item for env and alias
@@ -23,19 +26,21 @@ typedef struct Item
 {
 	char *name;
 	char *value;
+	char *next; /*linked list*/
 } Item;
 
 
 /*utilz.c -> S*/
 char *_strtok(char *str, const char *delim);
 size_t _getline(char **string, size_t *n, FILE *stream);
-/* get one token which is the command 
+/* get one token which is the command
 * ex. ls and return the path of ls command (usr/bin/ls or built_ins/ls) */
 char *get_built_in_path(char *command);
 
 /*handle_commands.c -> A*/
 int handle_commands(char *commands, Item *env, Item *alias, char *program_name);
 char **filter_commands(char *commands); /*Return: list of commands*/
+int handle_separators(int prev_result, char *separator); /*tells if the next command should be executed or not*/
 int handle_command(char **command, Item *env, Item *alias);
 int get_command_type(char *first_token);
 void handle_error(char *first_token, int error_id, char *program_name);
@@ -52,11 +57,10 @@ void free_items_list(Item *items);
 size_t get_items_len(Item *items);
 
 /*items2.c -> */
-Item *init_env(char **_env);
-Item *init_alias();
+Item *init_env(char **_env); /*the pointer must has a value even if it points to empty Item*/ 
+Item *init_alias(); /*the pointer must has a value even if it points to empty Item*/ 
 char *get_item_value(char *key);
-void add_item(char *key, char *value);
-void set_item(char *key, char *value);
+void set_item(char *key, char *value); /*if the item is already exist then will change its value*/
 void unset_item(char *key);
 
 #endif /*MAIN_H*/
