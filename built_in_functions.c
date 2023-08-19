da4c5a2b90780b203c6e472fd7da55e6ed987848
root <root@LAPTOP-LFKJQ0PM.localdomain>
2023-08-20 02:42:28 +0300

    Print ENV

--- built_in_functions.c
+++ built_in_functions.c
@@ -45,3 +45,18 @@ int _cd_(char *directory, char *program_name)
 	}
 	return (0);
 }
+/**
+ * _env_ - prints the environment variables
+ * Return: 0 on success
+ */
+int _env_(void)
+{
+	int i;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	return (0);
+}
