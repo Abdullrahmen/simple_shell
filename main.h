45c6b32b033ed20872decb63f8d4bdaa61d4fca2
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-08-21 02:03:55 +0300

    fix

--- main.h
+++ main.h
@@ -40,6 +40,7 @@ char *_strcpy(char *dest, char *src);
 size_t _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 char *_strdup(char *str);
+unsigned int _strspn(char *str, char *target);
 int _atoi(char *str);
 
 /*handle_commands.c -> A*/
