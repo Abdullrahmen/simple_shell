8ca62cdd9e3332279bcfbcecca19fba25afd5c42
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-08-22 16:27:21 +0300

    fix

--- nodes.c
+++ nodes.c
@@ -1,35 +1,6 @@
 #include "main.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list.
- * @head: the head of the list
- * @name: the name of the node
- * @value: the value of the node
- * Return: the address of the new element
- */
-Item *add_node(Item *head, char *name, char *value)
-{
-	Item *new_head = malloc(sizeof(Item));
-
-	if (!head || !new_head)
-	{
-		free(new_head);
-		return (NULL);
-	}
-	new_head->name = _strdup(name);
-	new_head->value = _strdup(value);
-	if (!new_head->name  || !new_head->value)
-	{
-		free(new_head->name);
-		free(new_head->value);
-		free(new_head);
-		return (NULL);
-	}
-	new_head->next = head;
-	return (new_head);
-}
-
-/**
  * delete_nodeint_at_index - deletes the node at index
  * @head: head of the list
  * @index: emplacement of the node to delet
