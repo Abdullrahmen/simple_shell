4b270f1be3c72802ed878c46e60b6c53d50ff9ab
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-08-22 14:47:02 +0300

    Fix functions

--- built_in_functions.c
+++ built_in_functions.c
@@ -7,24 +7,28 @@
 
 /**
  * _cd_ - changes the current directory
+ * @env: A list of environment variables
  * @directory: the directory required to change to it.
  * @program_name: the name of the program.
  * Return: 0 on success
  */
-int _cd_(char *directory, char *program_name)
+int _cd_(Item *env, char *directory, char *program_name)
 {
+	Item *env_iter = NULL;
 	char cwd[1024];
+	int value = 0;
 
+	env_iter = env;
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
 	{
-		perror("getcwd() error");
+		write(STDERR_FILENO, "getcwd() error", 15);
 		return (1);
 	}
 	if (directory == NULL || _strcmp(directory, "~") == 0)
-		directory = get_item_value("HOME");
+		directory = get_item_value(env, "HOME");
 	else if (_strcmp(directory, "-") == 0)
-		directory = get_item_value("OLDPWD");
-	if (chdir(directory) == -1)
+		directory = get_item_value(env, "OLDPWD");
+	if (chdir(directory) == -1;)
 	{
 		write(STDERR_FILENO, program_name, _strlen(program_name));
 		write(STDERR_FILENO, ": 1", 3);
@@ -37,10 +41,10 @@ int _cd_(char *directory, char *program_name)
 	}
 	else
 	{
-		_setenv_("OLDPWD", cwd);
+		_setenv_(env, "OLDPWD", cwd);
 		if (getcwd(cwd, sizeof(cwd)) == NULL)
 		{
-			perror("getcwd() error");
+			write(STDERR_FILENO, "getcwd() error", 15);
 			return (1);
 		}
 	}
@@ -79,103 +83,52 @@ int _setenv_(Item *env, char *name, char *value)
 {
 	Item *env_iter = NULL;
 
-	env_iter = malloc(sizeof(Item));
-	if (!env_iter)
-		perror("setenv ERROR");
 	env_iter = env;
+	if (!env_iter || !name)
+		write(STDERR_FILENO, "setenv ERROR", 13);
 	env_iter->name = name;
 	env_iter->value = value;
-	if (env_iter)
+	while (env_iter)
 	{
-		add_node(&env_iter, env_iter->name, env_iter->value);
+		if (_strcmp(name, env_iter->name))
+		{
+			env_iter = env_iter->next;
+			continue;
+		}
+		env_iter->value = _strdup(value);
+		return (0);
 	}
+	env = add_node(&env, name, value);
 	return (0);
 }
 
 /**
- * add_node - adds a new node at the beginning of a list_t list.
- * @head: the head of the list
- * @name: the name of the new variable
- * @value: the value of the new variable
- * Return: the address of the new element
+ * _unsetenv_ - unsets the environment variable
+ * @env: A list of the environment variable
+ * @name: the name of the variable
+ * Return: 0 on success
  */
-Item *add_node(Item **head, char *name, char *value)
-{
-	Item *new_head = malloc(sizeof(Item));
-
-	if (!head || !new_head)
-		return (NULL);
-	if (name)
-	{
-		new_head->name = _strdup(name);
-		new_head->value = _strdup(value);
-		if (!new_head->name  || !new_head->value)
-		{
-			free(new_head);
-			return (NULL);
-		}
-	}
-	new_head->next = *head;
-	*head = new_head;
-	return (new_head);
-}
-
 int _unsetenv_(Item *env, char *name)
 {
-	Item *node = env;
+	Item *env_iter = NULL;
 	unsigned int i = 0;
 	char *p;
 	int env_changed;
 
-	if (!node || !name)
-		return (0);
-
-	printf("l\n");
-	while (node)
+	env_iter = env;
+	if (!env_iter || !name)
+		write(STDERR_FILENO, "unsetenv ERROR", 15);
+	while (env_iter)
 	{
-		printf("O\n");
-		if (node->name != name)
+		if (env_iter->name == name)
 		{
-			printf("F\n");
 			env_changed = delete_nodeint_at_index(&env, i);
-			printf("G\n");
 			i = 0;
-			node = env;
+			env_iter = env;
 			continue;
 		}
-		node = node->next;
+		env_iter = env_iter->next;
 		i++;
 	}
-	printf("z\n");
 	return (env_changed);
-}
-
-int delete_nodeint_at_index(Item **head, unsigned int index)
-{
-	Item *current, *previous;
-	unsigned int counter = 0;
-
-	if (!head || !*head)
-		return (-1);
-	if (index == 0)
-	{
-		current = *head;
-		*head = (*head)->next;
-		free(current);
-		return (1);
-	}
-	current = *head;
-	while (current)
-	{
-		if (counter == index)
-		{
-			previous->next = current->next;
-			free(current);
-			return (1);
-		}
-		counter++;
-		previous = current;
-		current = current->next;
-	}
-	return (-1);
 }
