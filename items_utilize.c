1e0b2ff0e93373153f12b9a68ff8879f8d7b5903
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-08-24 14:44:27 +0000

    Another fix

--- items_utilize.c
+++ items_utilize.c
@@ -35,9 +35,9 @@ int name2value_2(size_t *i, size_t *len, char *token, char **value,
 		temp_str[0] = '\0';
 
 		sub_str = _substr(*str, *i);
-		_strcat(temp_str, sub_str);
-		_strcat(temp_str, *value);
-		_strcat(temp_str, &((*str)[*i + _strlen(token)]));
+		_str_concat(&temp_str, sub_str);
+		_str_concat(&temp_str, *value);
+		_str_concat(&temp_str, &((*str)[*i + _strlen(token)]));
 		free(sub_str);
 
 		free(*str);
@@ -52,6 +52,23 @@ int name2value_2(size_t *i, size_t *len, char *token, char **value,
 	return (1);
 }
 
+void alias_name2value(char **str, char **copied_str, size_t *len, char *token,
+						char *value, size_t *i)
+{
+	char *temp_str = NULL;
+
+	*len = _strlen(*str) - _strlen(token) + _strlen(value) + 1;
+	temp_str = malloc(sizeof(char) * *len);
+	temp_str[0] = '\0';
+	_str_concat(&temp_str, value);
+	_str_concat(&temp_str, &((*str)[_strlen(token)]));
+	free(*str);
+	*str = temp_str;
+	*i += _strlen(value) + 1;
+	free(*copied_str);
+	*copied_str = _strdup(*str);
+}
+
 /**
 * name2value - convert all env and alias Items in the string to their values
 * @str: the string
@@ -60,36 +77,33 @@ int name2value_2(size_t *i, size_t *len, char *token, char **value,
 */
 void name2value(char **str, Item *env, Item *alias)
 {
-	char *value = NULL, *token = NULL, *temp_str = NULL,
-					*copied_str = NULL;
-	size_t i = 0, len = 0;
+	char *value = NULL, *token = NULL, *copied_str = NULL;
+	size_t i = 0, len = 0, len2 = 0;
 
 	if (!str || !*str || !**str)
 		return;
-
 	copied_str = _strdup(*str);
+	len2 = _strlen(copied_str);
 	token = _strtok(copied_str, " ");
 	if (!token)
 		return;
+
 	value = get_item_value(alias, token);
-
 	if (value)
-	{
-		len = _strlen(*str) - _strlen(token) + _strlen(value) + 1;
-		temp_str = malloc(sizeof(char) * len);
-		temp_str[0] = '\0';
-
-		_strcat(temp_str, value);
-		_strcat(temp_str, &((*str)[_strlen(token)]));
-		free(*str);
-		*str = temp_str;
-
-		i += _strlen(value) + 1;
-		free(copied_str);
-		copied_str = _strdup(*str);
-	}
+		alias_name2value(str, &copied_str, &len, token, value, &i);
+	else
+		if (_strlen(token) >= len2)
+		{
+			free(copied_str);
+			return;
+		}
+		else
+			copied_str[_strlen(token)] = ' ';
+	len2 = _strlen(copied_str);
 	while (1)
 	{
+		if (i >= len2)
+			break;
 		token = _strtok(&(copied_str[i]), " ");
 		if (!token)
 			break;
